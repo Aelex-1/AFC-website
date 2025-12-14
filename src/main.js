3d59a8f897fd3e571db3c0127f338a18b7ff02e6
tagaum98 <a.aquino.500413@umindanao.edu.ph>
2025-12-14 22:40:01 +0800

    First Commit

---
+++ src/main.js
@@ -0,0 +1,103 @@
+import '../styles/modern-noramlize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/footer.css';
+import '../styles/components/home.css';
+import '../styles/components/about.css';
+import '../styles/components/franchise.css';
+import '../styles/components/locations.css';
+import '../styles/components/blogs.css';
+import '../styles/components/extras.css';
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import '../styles/components/swiper.css';
+import '../styles/utils.css';
+
+
+const navbar = document.getElementById('sidebar');
+function openSidebar() {
+    navbar.classList.add('show');
+
+}
+function closeSidebar() {
+    navbar.classList.remove('show');
+}
+window.openSidebar = openSidebar;
+window.closeSidebar = closeSidebar;
+
+
+const swiper = new Swiper('.home-swiper', {
+    // Optional parameters
+    loop: true,
+
+    autoplay: {
+    delay: 4000,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, 
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+const swiperBlogs = new Swiper('.blogs-swiper', {
+    // loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: { 
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        }
+    }
+})
+
+const aboutSwiper = new Swiper('.about-swiper', {
+    loop: true,
+    spaceBetween: 20,
+
+    autoplay: {
+    delay: 4000,
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+    
+})
+
+document.querySelectorAll("details").forEach((details) => {
+  details.addEventListener("toggle", () => {
+    if (details.open) {
+      document.querySelectorAll("details").forEach((other) => {
+        if (other !== details) {
+          other.open = false;
+        }
+      });
+    }
+  });
+});
